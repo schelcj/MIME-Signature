9ebd45cf4ebc5a2dbbd0b2207964dab52751a046
Martin H. Sluka <martin@sluka.de>
2018-08-08 15:27:18 +0200

    added test for Changes file

--- lib/MIME/Signature.pm
+++ lib/MIME/Signature.pm
@@ -3,7 +3,7 @@ package MIME::Signature;
 use 5.014;
 use warnings;
 
-our $VERSION = '0.13';
+our $VERSION = '0.14';
 
 use Carp qw(croak);
 use Encode qw(decode encode encode_utf8);
@@ -285,7 +285,7 @@ MIME::Signature - appends signature to mail messages
 
 =head1 VERSION
 
-Version 0.13
+Version 0.14
 
 =cut
 
